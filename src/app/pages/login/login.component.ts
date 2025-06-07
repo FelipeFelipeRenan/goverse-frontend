3e32a949a5fa878d50f23b794c5c6a22ef783e6d
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-07 17:36:55 -0300

    feat: adding login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,  // importante: componente standalone
+  imports: [ReactiveFormsModule],  // importa aqui
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(event : Event) {
+    if (this.loginForm.invalid) return;
+    const { email, password } = this.loginForm.value;
+    console.log('Login:', email, password);
+    alert("Login feito");
+  }
+
+    onGoogleLogin() {
+    alert('Login com Google - implementar fluxo OAuth');
+    // Aqui você pode iniciar o fluxo OAuth com seu backend, redirecionar para o endpoint ou abrir popup
+  }
+}
