b048b826699773cd2d7934bc900fa1c05b1bd1c9
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-09 15:57:54 -0300

    feat: adding auth to login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,  // importante: componente standalone
-  imports: [ReactiveFormsModule],  // importa aqui
+  standalone: true, 
+  imports: [ReactiveFormsModule],  
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-    });
-  }
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   onSubmit(event : Event) {
-    event.preventDefault();
-    if (this.loginForm.invalid) return;
-    const { email, password } = this.loginForm.value;
-    console.log('Login:', email, password);
-    alert("Login feito");
+    event.preventDefault()
+
+    this.authService.login({email: this.email, password: this.password}).subscribe({
+      next: (response) => {
+        this.authService.saveToken(response.token);
+        this.router.navigate(['/home'])
+      },
+      error: (err) => {
+        console.error('Erro de login:', err)
+        this.errorMessage = "E-mail ou senha inválidos"
+      }
+    });
   }
 
     onGoogleLogin() {
