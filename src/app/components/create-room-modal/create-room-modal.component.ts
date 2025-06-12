cb05c3b213a5ed339630672abca779b9588c698f
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-12 15:16:16 -0300

    feat: adding create-room-modal service

--- src/app/components/create-room-modal/create-room-modal.component.ts
+++ src/app/components/create-room-modal/create-room-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-room-modal',
@@ -8,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class CreateRoomModalComponent {
 
+    @Output() close = new EventEmitter<void>();
+    @Output() roomCreated = new EventEmitter<any>();
+
+    roomForm: FormGroup;
+
+    constructor(private fb: FormBuilder){
+        this.roomForm = this.fb.group({
+            room_name: ['', Validators.required],
+            room_description: [''],
+            max_members: [10,[Validators.required, Validators.min(2)]],
+            is_public: [true]
+        });
+    }
+
+    onSubmit(){
+        if(this.roomForm.valid){
+            this.roomCreated.emit(this.roomForm.value);
+            this.close.emit();
+        }
+    }
+
+    onCancel(){
+        this.close.emit();
+    }
 }
