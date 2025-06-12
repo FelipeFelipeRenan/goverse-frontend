28b4aaf1a720c59966425df23aeca318600b5fc4
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-12 15:07:53 -0300

    feat: adding logout and create room to home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Room, RoomService } from '../../services/room.service';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +16,7 @@ export class HomeComponent implements OnInit {
   ownedRooms: Room[] = [];
   joinedRooms: Room[] = [];
 
-  constructor(private roomService: RoomService) {}
+  constructor(private roomService: RoomService, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.roomService.getOwnedRooms().subscribe({
@@ -27,4 +29,14 @@ export class HomeComponent implements OnInit {
       error: err => console.error('Erro ao carregar salas que participa:', err)
     });
   }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(["/login"])
+  }
+
+  onCreateRoom(){
+    this.router.navigate(["/create-room"])
+  }
+
 }
