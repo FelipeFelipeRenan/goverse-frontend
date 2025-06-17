9bfea7eff73fe8b393d5347d4fadea0641796e10
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-17 15:27:57 -0300

    feat: bootstrapping room members component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RoomMembersComponent } from './rooms/room-members/room-members.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
 
-  { 
+  {
     path: 'home',
     loadComponent: () =>
       import('./pages/home/home.component').then(m => m.HomeComponent)
 
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: 'rooms/:roomID/members', component: RoomMembersComponent}
 ];
---
+++ src/app/rooms/room-members/room-members.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-members',
+  imports: [],
+  templateUrl: './room-members.component.html',
+  styleUrl: './room-members.component.css'
+})
+export class RoomMembersComponent {
+
+}
