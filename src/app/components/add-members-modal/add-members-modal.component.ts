92da24747cf8a62de89cc0c485c2252511ad54a4
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-19 17:40:28 -0300

    feat: bootstrapping add members modal

---
+++ src/app/components/add-members-modal/add-members-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-members-modal',
+  imports: [],
+  templateUrl: './add-members-modal.component.html',
+  styleUrl: './add-members-modal.component.css'
+})
+export class AddMembersModalComponent {
+
+}
